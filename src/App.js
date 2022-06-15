ab62d920cfe52aac95c41ffe522b91a789cfe025
Amir Sarfraz <amirsarfarz50@gmail.com>
2022-06-16 00:16:52 +0500

    create navbar and textform

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-let name='amirsarfraz';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
-    <><nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <h1>{name}</h1>
-    </nav><div className='container'>
-   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-     when an unknown printer took a galley of type and scrambled it to make a type specimen books.</p>
-      </div></>
+<>
+<Navbar title="amirsarfraz"/>
+<div className='container'>
+<TextForm />
+</div>
+</>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+function TextForm() {
+    return(
+        <>
+        <div className="form-floating">
+  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
+  <label for="floatingTextarea2">Comments</label>
+</div>
+        
+        </>
+
+    );
+}
+export default TextForm;
