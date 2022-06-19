28e6a31e308a815aaf5ef8c7bff32137b212ecdd
Amir Sarfraz <amirsarfarz50@gmail.com>
2022-06-20 01:05:12 +0500

    dark and white background color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 function App() {
   return (
 <>
 <Navbar title="amirsarfraz"/>
 <div className='container'>
 <TextForm heading ="Enter the text to analiyze" />
+<About />
 </div>
 </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,13 +8,23 @@ function TextForm(props) {
     const handleOnChange = (event) =>{
         setText(event.target.value);
     }
+    const  handleUpClear = () =>{
+        setText('');
+    }
+    const handleInClick = () =>{
+        setText(text.toLocaleLowerCase());
+    }
     return(
         <>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="3"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-primary mb-13" onClick={handleUpClick}>Convert to uppercase</button><br /><br/>
+        <button className="btn btn-primary" onClick={handleInClick}>Convert to lowercase</button><br /><br/>
+        <br /><br/>
+        <button className="btn btn-primary" onClick={handleUpClear}>Clear all text</button>
+
         <div className="container my-3">
         <h1>your text Summary</h1>
         <p>{text.split(" ").length} words and{text.length} character</p>
