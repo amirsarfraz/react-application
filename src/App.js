42b51a67e5f53959fd678c749bc1086d7a0a6928
Amir Sarfraz <amirsarfarz50@gmail.com>
2022-06-21 00:49:35 +0500

    alert components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
+import { useState } from 'react';
+import Alert from './components/Alert';
 function App() {
+  const [mode, setMode] = useState('dark');
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743'
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
 <>
-<Navbar title="amirsarfraz"/>
+<Navbar title="amirsarfraz" mode={mode} toggleMode={toggleMode}/>
 <div className='container'>
-<TextForm heading ="Enter the text to analiyze" />
-<About />
+  <Alert alert="This is Alert"/>
+<TextForm heading ="Enter the text to analiyze" mode={mode} />
+{/* <About /> */}
 </div>
 </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,9 +16,11 @@ function TextForm(props) {
     }
     return(
         <>
+        <div className="conatiner" style={{color:props.mode ==='dark'?'white': 'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="3"></textarea>
+        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange}
+         style={{backgroundColor:props.mode ==='dark'?'gray': 'white', color: props.mode ==='dark'?'white': 'black'}} rows="3"></textarea>
         </div>
         <button className="btn btn-primary mb-13" onClick={handleUpClick}>Convert to uppercase</button><br /><br/>
         <button className="btn btn-primary" onClick={handleInClick}>Convert to lowercase</button><br /><br/>
@@ -30,7 +32,8 @@ function TextForm(props) {
         <p>{text.split(" ").length} words and{text.length} character</p>
         <p>{0.008 * text.split("").length} Minutes read</p>
         <h3>preview</h3>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter something in the textbox about to preview it here"}</p>
+        </div>
         </div>
         </>
 
